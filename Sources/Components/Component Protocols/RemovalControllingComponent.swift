3569fa9505a1e71084d1335795ec7f5f78dd1309
cocoatoucher <cocoatoucher@posteo.se>
2020-05-10 11:47:01 +0200

    Fix documentation.

--- Sources/Components/Component Protocols/RemovalControllingComponent.swift
+++ Sources/Components/Component Protocols/RemovalControllingComponent.swift
@@ -30,11 +30,10 @@ import Foundation
 /// Entity will be removed if at least one of its `RemovalControllingComponent`s
 /// return `true` for its `canEntityBeRemoved` value.
 /// An example scenario where this might be useful is, an entity needs
-/// to be removed but there is be an explosion animation that should be
-/// played.
+/// to be removed but there needs to be an explosion animation played.
 /// Adoption of this protocol by the component which will play this
 /// animation ensures that the entity will not be removed until the
-/// animation has done playing.
+/// animation is done playing.
 public protocol RemovalControllingComponent {
     
     /// `true` if the component is ready for removal of its entity.
